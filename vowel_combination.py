d69785f3a5776b20bfac5a08c8d4ec19a5e436d4
Pramod Kumar <pramodkumar.damam73@gmail.com>
2020-04-09 18:18:34 +0530

    Optimise vowel problem

--- vowel_combination.py
+++ vowel_combination.py
@@ -1,17 +1,16 @@
 def get_next_state(state):
-    next_starts_with = [state[0], 0, 0, 0, 0]
-    starts_with = state[1]
+    next_starts_with = [sum(state), 0, 0, 0, 0]
     for i in range(1, 5):
-        next_starts_with[i] = sum(starts_with[i:])
-    return (sum(next_starts_with), next_starts_with)
+        next_starts_with[i] = sum(state[i:])
+    return next_starts_with
 
 def get_combinations(n: int) -> int:
     if n == 1:
         return 5
-    state = (15, [5, 4, 3, 2, 1])
-    for i in range(1, n):
+    state = [5, 4, 3, 2, 1]
+    for i in range(1, n-1):
         state = get_next_state(state)
-    return state[0]
+    return sum(state)
 
 
 print(get_combinations(5))
