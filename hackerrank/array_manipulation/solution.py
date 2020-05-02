d4639cb989029bc34e03a4e53bebafa4a3dce83a
Pramod Kumar <pramodkumar.damam73@gmail.com>
2020-05-02 11:04:12 +0530

    Add array manipulation from hackerrank

---
+++ hackerrank/array_manipulation/solution.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the arrayManipulation function below.
+def arrayManipulation(n, queries):
+    arr = [0] * (n+1)
+    for q in queries:
+        arr[q[0]-1] += q[2]
+        arr[q[1]] -= q[2]
+    tmp, ans = 0, 0
+    for i in range(len(arr)):
+        tmp += arr[i]
+        ans = max(ans, tmp)
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
